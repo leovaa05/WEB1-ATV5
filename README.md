8e5a78a1508b53311d2e6c5e6d7a93df90bf34d5
leovaa <lvaa@aluno.ifnmg.edu.br>
2025-07-10 19:06:41 +0000

    Estrutura Inicial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WEB1-ATV5
 Jogo Show do Milhão feito em JavaScript
+# Nome Completo:
+Leonardo Viana Alves de Amorim
+# Regras do Jogo:
+# Como Jogar:
+# Como Executar:
+# Créditos:
+# Licença:
---
+++ showdomilhao.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("!!Bem-vindo ao Show do Milhão!!");
+rl.close();
